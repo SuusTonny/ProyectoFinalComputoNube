02a0fa3abb4ac6c319e44164384178dfd45318c9
SuusTonny <ja.cisnerosrivera@ugto.mx>
2022-12-08 14:44:48 -0600

    cambiar puerto

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ config();
 //console.log();
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://SuusTony:admin12@cluster0.wqvv7a6.mongodb.net/?retryWrites=true&w=majority"
 
-export  const PORT = process.env.PORT || 80;
+export  const PORT = process.env.PORT || 443;
